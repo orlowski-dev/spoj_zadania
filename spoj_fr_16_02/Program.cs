744d6804e0b831a59bda102b83d945edc1b1d380
Patryk Orłowski <80420442+orlowski-dev@users.noreply.github.com>
2022-12-19 10:16:18 +0100

    Dodano zadanie FR_16_02
    https://pl.spoj.com/problems/FR_16_02/

---
+++ spoj_fr_16_02/Program.cs
@@ -0,0 +1,23 @@
+﻿uint iloscZapisow = Convert.ToUInt32(Console.ReadLine());
+int[] wysokosci = new int[iloscZapisow];
+string wysokosciInp = Console.ReadLine();
+uint index = 0;
+int tmpA, tmpB, tmpC;
+uint iloscWzniesien = 0;
+
+foreach (string inp in wysokosciInp.Split(" "))
+{
+    wysokosci[index] = Convert.ToInt32(inp);
+    index++;
+}
+
+for (int i = 0; i < wysokosci.Length - 2; i++)
+{
+    tmpA = wysokosci[i];
+    tmpB = wysokosci[i+1];
+    tmpC = wysokosci[i+2];
+
+    if (tmpA < tmpB && tmpB > tmpC) { iloscWzniesien++; }
+}
+
+Console.WriteLine(iloscWzniesien);
