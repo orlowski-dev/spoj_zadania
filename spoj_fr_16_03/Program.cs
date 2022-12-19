7897910e8323f7e25b3b3e893d60f7f5ee20a41a
Patryk Orłowski <80420442+orlowski-dev@users.noreply.github.com>
2022-12-19 10:40:37 +0100

    FR_16_03 - Snake case na camel case
    https://pl.spoj.com/problems/FR_16_03/

---
+++ spoj_fr_16_03/Program.cs
@@ -0,0 +1,15 @@
+﻿string inp = Console.ReadLine();
+string nextChar;
+
+for (int i = 0; i < inp.Length; i++)
+{
+    if (inp[i].Equals('_'))
+    {
+        nextChar = Convert.ToString(inp[i + 1]);
+        Console.Write(nextChar.ToUpper());
+        i++;
+    } else
+    {
+        Console.Write(inp[i]);
+    }
+}
